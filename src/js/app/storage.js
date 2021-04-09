2ee516ab66bc992d7eb5b77305d24e4efc58c8dc
nicross <nicross@users.noreply.github.com>
2021-04-09 09:19:38 -0500

    Port app.storage from S.E.A.

---
+++ src/js/app/storage.js
@@ -0,0 +1,62 @@
+app.storage = (() => {
+  const isSupported = 'localStorage' in window
+
+  const storage = isSupported
+    ? window.localStorage
+    : {
+        data: {},
+        getItem: (key) => this.data[key],
+        removeItem: (key) => delete this.data[key],
+        setItem: (key) => this.data[key] = value,
+      }
+
+  const gameKey = 'exo_game',
+    settingsKey = 'exo_settings',
+    statsKey = 'exo_stats',
+    versionKey = 'exo_version'
+
+  function get(key) {
+    try {
+      const value = storage.getItem(key)
+      return JSON.parse(value)
+    } catch (e) {}
+  }
+
+  function remove(key) {
+    return storage.removeItem(key)
+  }
+
+  function set(key, value) {
+    try {
+      storage.setItem(key, JSON.stringify(value))
+    } catch (e) {}
+  }
+
+  return {
+    clearGame: function () {
+      remove(gameKey)
+      return this
+    },
+    getGame: () => get(gameKey) || {},
+    getSettings: () => get(settingsKey) || {},
+    getStats: () => get(statsKey) || {},
+    getVersion: () => get(versionKey) || '0.0.0',
+    hasGame: () => Boolean(get(gameKey)),
+    setGame: function (value) {
+      set(gameKey, value)
+      return this
+    },
+    setStats: function (value) {
+      set(statsKey, value)
+      return this
+    },
+    setSettings: function (value) {
+      set(settingsKey, value)
+      return this
+    },
+    setVersion: function (value) {
+      set(versionKey, value)
+      return this
+    },
+  }
+})()
