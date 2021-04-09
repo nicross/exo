5be2ecb01c376ce75179614db7a11e8de485b584
nicross <nicross@users.noreply.github.com>
2021-04-09 09:20:13 -0500

    Port app.state modules from S.E.A.

---
+++ src/js/app/state/game.js
@@ -0,0 +1,81 @@
+app.state.game = engine.utility.machine.create({
+  state: 'initial',
+  transition: {
+    initial: {
+      activate: function () {
+        this.change('none')
+      },
+    },
+    none: {
+      load: function () {
+        this.change('running')
+      },
+      new: function () {
+        this.change('running')
+      },
+    },
+    paused: {
+      exit: function () {
+        this.change('none')
+      },
+      resume: function () {
+        this.change('running')
+      },
+    },
+    running: {
+      pause: function () {
+        this.change('paused')
+      },
+    },
+  },
+})
+
+engine.ready(() => {
+  // Initial mix
+  engine.audio.mixer.master.param.gain.value = engine.const.zeroGain
+
+  // Game state changes
+  app.state.screen.on('before-game-pause', () => app.state.game.dispatch('pause'))
+  app.state.screen.on('before-gameMenu-mainMenu', () => app.state.game.dispatch('exit'))
+  app.state.screen.on('before-gameMenu-resume', () => app.state.game.dispatch('resume'))
+  app.state.screen.on('before-mainMenu-continue', () => app.state.game.dispatch('load'))
+  app.state.screen.on('before-mainMenu-newGame', () => app.state.game.dispatch('new'))
+
+  app.state.game.dispatch('activate')
+})
+
+app.state.game.on('before-none-load', () => {
+  engine.state.import(
+    app.storage.getGame()
+  )
+})
+
+app.state.game.on('before-none-new', () => {
+  app.storage.clearGame()
+
+  engine.state.import({
+    position: {
+      quaternion: engine.utility.quaternion.identity(),
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    seed: Math.random(),
+  })
+
+  app.autosave.trigger()
+})
+
+app.state.game.on('enter-paused', () => {
+  engine.audio.ramp.linear(engine.audio.mixer.master.param.gain, engine.const.zeroGain, 0.5)
+
+  app.autosave.disable().trigger()
+  engine.loop.pause()
+})
+
+app.state.game.on('enter-running', () => {
+  engine.audio.ramp.linear(engine.audio.mixer.master.param.gain, 1, 0.5)
+
+  app.autosave.enable()
+  engine.loop.resume()
+})
---
+++ src/js/app/state/screen.js
@@ -0,0 +1,125 @@
+app.state.screen = engine.utility.machine.create({
+  state: 'none',
+  transition: {
+    game: {
+      pause: function () {
+        this.change('gameMenu')
+      },
+    },
+    gameMenu: {
+      mainMenu: function () {
+        this.change('mainMenu')
+      },
+      misc: function () {
+        this.change('misc')
+      },
+      quit: () => {
+        ElectronApi.quit()
+      },
+      resume: function () {
+        this.change('game')
+      },
+      status: function () {
+        this.change('status')
+      },
+    },
+    mainMenu: {
+      continue: function () {
+        this.change('game')
+      },
+      misc: function () {
+        this.change('misc')
+      },
+      newGame: function () {
+        this.change('game')
+      },
+      quit: () => {
+        ElectronApi.quit()
+      },
+    },
+    misc: {
+      back: function () {
+        if (app.state.game.is('none')) {
+          this.change('mainMenu')
+        } else {
+          this.change('gameMenu')
+        }
+      },
+    },
+    none: {
+      activate: function () {
+        this.change('splash')
+      },
+    },
+    settings: {
+      back: function () {
+        this.change('misc')
+      },
+    },
+    stats: {
+      back: function () {
+        this.change('misc')
+      },
+    },
+    status: {
+      back: function () {
+        this.change('gameMenu')
+      },
+    },
+    splash: {
+      start: function () {
+        this.change('mainMenu')
+      },
+    },
+  },
+})
+
+engine.ready(() => {
+  [...document.querySelectorAll('.a-app--screen')].forEach((element) => {
+    element.setAttribute('aria-hidden', 'true')
+    element.setAttribute('role', 'persentation')
+  })
+
+  app.state.screen.dispatch('activate')
+})
+
+app.state.screen.on('exit', (e) => {
+  const active = document.querySelector('.a-app--screen-active')
+  const inactive = document.querySelector('.a-app--screen-inactive')
+
+  if (active) {
+    active.classList.remove('a-app--screen-active')
+    active.classList.add('a-app--screen-inactive')
+    active.setAttribute('aria-hidden', 'true')
+    active.setAttribute('role', 'persentation')
+  }
+
+  if (inactive) {
+    inactive.classList.remove('a-app--screen-inactive')
+    inactive.hidden = true
+  }
+})
+
+app.state.screen.on('enter', (e) => {
+  const selectors = {
+    game: '.a-app--game',
+    gameMenu: '.a-app--gameMenu',
+    mainMenu: '.a-app--mainMenu',
+    misc: '.a-app--misc',
+    settings: '.a-app--settings',
+    splash: '.a-app--splash',
+    stats: '.a-app--stats',
+    status: '.a-app--status',
+  }
+
+  const selector = selectors[e.currentState]
+  const element = document.querySelector(selector)
+
+  element.removeAttribute('aria-hidden')
+  element.removeAttribute('role')
+  element.removeAttribute('hidden')
+
+  window.requestAnimationFrame(() => {
+    element.classList.add('a-app--screen-active')
+  })
+})
