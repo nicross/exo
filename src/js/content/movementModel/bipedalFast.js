c5df9c8b0b1677321cfafaa48baf346a9712f79a
nicross <nicross@users.noreply.github.com>
2021-04-10 10:49:00 -0500

    Reduce lateral movement and turning while sprinting

--- src/js/content/movementModel/bipedalFast.js
+++ src/js/content/movementModel/bipedalFast.js
@@ -3,14 +3,14 @@ content.movementModel.bipedalFast = {
     return {
       angularAcceleration: Math.PI / 2,
       angularDeceleration: Math.PI * 2,
-      angularVelocity: Math.PI / 2,
+      angularVelocity: Math.PI / 4,
       lateralAcceleration: 5,
       lateralDeceleration: 10,
       lateralVelocity: 10,
       reference: this,
       rotateScale: 1,
       xScale: 1,
-      yScale: 1,
+      yScale: 0.5,
     }
   },
 }
