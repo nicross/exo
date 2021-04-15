c590dbe1d796ea58141cdf0add796d1f0eaa3e27
nicross <nicross@users.noreply.github.com>
2021-04-15 14:58:10 -0500

    Implement basic piston sound

--- src/js/content/audio/footstep.js
+++ src/js/content/audio/footstep.js
@@ -1,10 +1,11 @@
 content.audio.footstep = (() => {
   const bus = content.audio.createBus(),
+    pistonRoot = engine.utility.midiToFrequency(31),
     textureField = engine.utility.createPerlinWithOctaves(engine.utility.perlin2d, ['footstep', 'texture'], 4),
     textureScale = 100
 
   let isLeft = false,
-    lastAngle = engine.utility.vector3d.create(),
+    lastAngle = 0,
     lastStep = engine.utility.vector3d.create()
 
   bus.gain.value = engine.utility.fromDb(-6)
@@ -32,17 +33,16 @@ content.audio.footstep = (() => {
       return true
     }
 
-    const angle = engine.position.getQuaternion().forward(),
-      dot = angle.dotProduct(lastAngle),
-      theta = Math.acos(dot / 1)
+    const angle = engine.position.getEuler().yaw,
+      difference = angle - lastAngle
 
-    return !engine.utility.between(theta, -Math.PI/2, Math.PI/2)
+    return !engine.utility.between(difference, -Math.PI/2, Math.PI/2)
   }
 
   function trigger() {
     const binaural = engine.audio.binaural.create({
-      x: 0,
-      y: (isLeft ? 1 : -1) * 0.5,
+      x: 0.25,
+      y: (isLeft ? 1 : -1) * 0.25,
     }).to(bus)
 
     triggerPiston(binaural)
@@ -67,22 +67,42 @@ content.audio.footstep = (() => {
     const thrust = content.movement.normalThrust().distance()
     const strength = humanizeNormal(thrust, 1/16)
 
-    // TODO: synth
-    console.log('piston', strength)
+    const synth = engine.audio.synth.createSimple({
+      detune: engine.utility.random.float(-25, 25),
+      frequency: pistonRoot,
+      type: 'triangle',
+    }).shaped(
+      engine.audio.shape.noise4()
+    ).filtered({
+      frequency: pistonRoot * 4,
+      type: 'bandpass',
+    })
 
-    return engine.utility.timing.promise(250)
+    const detune = engine.utility.lerp(500, 1200, strength),
+      duration = engine.utility.lerp(1/2, 1/4, strength),
+      gain = engine.utility.fromDb(engine.utility.lerp(-6, -3, strength)),
+      now = engine.audio.time()
+
+    synth.param.detune.linearRampToValueAtTime(detune, now + duration)
+    synth.param.gain.linearRampToValueAtTime(gain, now + duration/2)
+    synth.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+    binaural.from(synth)
+    synth.stop(now + duration)
+
+    return engine.utility.timing.promise(duration / 2)
   }
 
   return {
     import: function () {
       isLeft = Math.random() > 0.5
-      lastAngle = engine.position.getQuaternion().forward()
+      lastAngle = engine.position.getEuler().yaw
       lastStep = engine.position.getVector()
       return this
     },
     reset: function () {
       isLeft = false
-      lastAngle = engine.utility.vector3d.create()
+      lastAngle = 0
       lastStep = engine.utility.vector3d.create()
       return this
     },
@@ -91,11 +111,11 @@ content.audio.footstep = (() => {
         return this
       }
 
-      if (shouldStep()) {
+      if (force || shouldStep()) {
         trigger()
 
         isLeft = !isLeft
-        lastAngle = engine.position.getQuaternion().forward()
+        lastAngle = engine.position.getEuler().yaw
         lastStep = engine.position.getVector()
       }
 
