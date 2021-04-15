8bbfff5eb4f1f480576c60332cf9dd45ce6d4276
nicross <nicross@users.noreply.github.com>
2021-04-15 12:48:38 -0500

    Implement basic collision sound

--- src/js/content/audio/collision.js
+++ src/js/content/audio/collision.js
@@ -1,10 +1,33 @@
 content.audio.collision = (() => {
   const bus = content.audio.createBus()
 
-  bus.gain.value = engine.utility.fromDb(-12)
-
   function play() {
+    const velocity = engine.position.getVelocity()
 
+    // XXX: strength is ratio to hardcoded maximum velocity
+    const distance = velocity.distance(),
+      normal = velocity.scale(1 / distance),
+      strength = engine.utility.clamp(distance / 20, 0, 1)
+
+    const synth = engine.audio.synth.createBuffer({
+      buffer: engine.audio.buffer.noise.pink(),
+    }).filtered({
+      frequency: engine.utility.lerpExp(200, 800, strength, 2),
+    })
+
+    const binaural = engine.audio.binaural.create({
+      ...normal,
+    }).from(synth).to(bus)
+
+    const duration = engine.utility.lerpExp(1, 4, strength, 0.5),
+      now = engine.audio.time()
+
+    synth.param.gain.setValueAtTime(engine.const.zeroGain, now)
+    synth.param.gain.exponentialRampToValueAtTime(1, now + 1/32)
+    synth.param.gain.exponentialRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+    synth.stop(now + duration)
+    setTimeout(() => binaural.destroy(), duration * 1000)
   }
 
   return {
@@ -16,5 +39,6 @@ content.audio.collision = (() => {
 })()
 
 engine.ready(() => {
-  content.movement.on('collision', () => content.audio.collision.trigger())
+  // TODO: play when first gluing from air
+  content.movement.on('reflect', () => content.audio.collision.trigger())
 })
