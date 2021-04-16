f0d8d46a356b6beecba7b14ef1a979240c7aab3c
nicross <nicross@users.noreply.github.com>
2021-04-16 11:42:41 -0500

    Port boosted dynamic range from S.E.A.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 engine.ready(() => {
   engine.loop.start().pause()
   app.activate()
+
+  // Boosted dynamic range
+  engine.audio.mixer.master.param.limiter.attack.value = 0.003
+  engine.audio.mixer.master.param.limiter.gain.value = 1
+  engine.audio.mixer.master.param.limiter.knee.value = 15
+  engine.audio.mixer.master.param.limiter.ratio.value = 15
+  engine.audio.mixer.master.param.limiter.release.value = 0.125
+  engine.audio.mixer.master.param.limiter.threshold.value = -30
 })
