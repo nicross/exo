dc01a2a1d526f29d56a255a053fafee79647cc41
nicross <nicross@users.noreply.github.com>
2021-04-18 13:42:11 -0500

    Implement metal material sound

--- src/js/content/prop/material/metal.js
+++ src/js/content/prop/material/metal.js
@@ -1,6 +1,38 @@
 content.prop.material.metal = content.prop.material.base.invent({
   name: 'material/metal',
   createSynth: function () {
+    const protons = engine.utility.scale(this.type.protons, 1, 108, 0, 1)
 
+    this.synth = engine.audio.synth.createAm({
+      carrierFrequency: this.rootFrequency,
+      carrierGain: 7/8,
+      gain: 2/3,
+      modDepth: 1/8,
+      modFrequency: 60,
+      modType: 'square',
+    }).filtered({
+      frequency: this.rootFrequency * 2,
+    }).connect(this.output)
+
+    const lfoDepth = engine.audio.synth.createLfo({
+      depth: 1/32,
+      frequency: engine.utility.lerp(1, 8, protons),
+    }).connect(this.synth.param.mod.depth)
+
+    this.synth.chainStop(lfoDepth)
+
+    const invert = engine.audio.circuit.invert({
+      from: lfoDepth,
+      to: this.synth.param.carrierGain,
+    })
+
+    this.synth.chainStop(invert)
+
+    const lfoGain = engine.audio.synth.createLfo({
+      depth: 1/3,
+      frequency: engine.utility.lerp(1/8, 1, protons),
+    }).connect(this.synth.param.gain)
+
+    this.synth.chainStop(lfoGain)
   },
 })
