04e85434effe9aede57540c19e759362608cce45
nicross <nicross@users.noreply.github.com>
2021-04-22 17:33:53 -0500

    Outline unlockable air abilities

---
+++ src/js/content/upgrades/rcsThrusters.js
@@ -0,0 +1,50 @@
+content.upgrades.rcsThrusters = content.upgrades.invent({
+  name: 'RCS Thrusters',
+  describe: function (level = this.level) {
+    if (!level) {
+      return ''
+    }
+
+    if (level == 1) {
+      return 'Unlocks air turning'
+    }
+
+    return `+${Math.round(this.levels[level].bonus / (Math.PI / 2) * 100)}% air turning`
+  },
+  levels: [
+    {
+      bonus: Math.PI / 8,
+      cost: {
+        'common/oxygen': 5,
+        'common/silicon': 5,
+        'metal/copper': 1,
+        'metal/gold': 1,
+      },
+      name: 'RCS Thrusters +1',
+    },
+    {
+      bonus: Math.PI / 4,
+      cost: {
+        'common/oxygen': 10,
+        'common/silicon': 10,
+        'metal/copper': 5,
+        'metal/gold': 5,
+        'exotic/uranium': 1,
+        'xenotech/artifact': 1,
+      },
+      name: 'RCS Thrusters +2',
+    },
+    {
+      bonus: Math.PI / 2,
+      cost: {
+        'common/oxygen': 20,
+        'common/silicon': 20,
+        'metal/copper': 10,
+        'metal/gold': 10,
+        'exotic/uranium': 5,
+        'xenotech/artifact': 2,
+      },
+      name: 'RCS Thrusters +3',
+    },
+  ],
+})
---
+++ src/js/content/upgrades/vectors.js
@@ -0,0 +1,50 @@
+content.upgrades.vectors = content.upgrades.invent({
+  name: 'Vectors',
+  describe: function (level = this.level) {
+    if (!level) {
+      return ''
+    }
+
+    if (level == 1) {
+      return 'Unlocks air strafing'
+    }
+
+    return `+${Math.round(this.levels[level].bonus / (Math.PI / 2) * 100)}% air strafing`
+  },
+  levels: [
+    {
+      bonus: Math.PI / 8,
+      cost: {
+        'common/carbon': 5,
+        'common/silicon': 5,
+        'metal/copper': 1,
+        'metal/silver': 1,
+      },
+      name: 'Vectors',
+    },
+    {
+      bonus: Math.PI / 4,
+      cost: {
+        'common/carbon': 10,
+        'common/silicon': 10,
+        'metal/copper': 5,
+        'metal/silver': 5,
+        'exotic/thorium': 1,
+        'xenotech/artifact': 1,
+      },
+      name: 'Vectors +1',
+    },
+    {
+      bonus: Math.PI / 2,
+      cost: {
+        'common/carbon': 20,
+        'common/silicon': 20,
+        'metal/copper': 10,
+        'metal/silver': 10,
+        'exotic/thorium': 5,
+        'xenotech/artifact': 2,
+      },
+      name: 'Vectors +2',
+    },
+  ],
+})
