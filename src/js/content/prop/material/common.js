8e7fc7a44e9303265d0da4c763623960b30ebdf4
nicross <nicross@users.noreply.github.com>
2021-04-24 20:30:02 -0500

    Revise common material sound

--- src/js/content/prop/material/common.js
+++ src/js/content/prop/material/common.js
@@ -3,27 +3,24 @@ content.prop.material.common = content.prop.material.base.invent({
   createSynth: function () {
     const protons = engine.utility.scale(this.type.protons, 1, 36, 0, 1)
 
-    this.synth = engine.audio.synth.createFm({
-      carrierFrequency: this.rootFrequency / 2,
-      carrierType: 'sine',
+    this.synth = engine.audio.synth.createPwm({
+      frequency: this.rootFrequency,
       gain: 2/3,
-      modDepth: this.rootFrequency,
-      modFrequency: this.rootFrequency * 2,
-      modType: 'triangle',
-    }).chainAssign('phaser', engine.audio.effect.createPhaser({
-      dry: 0,
-      feedback: 1/3,
-      frequency: engine.utility.lerp(1, 8, protons),
-      wet: 1,
-    })).filtered({
+      width: 0.5,
+    }).filtered({
       frequency: this.rootFrequency,
     }).connect(this.output)
 
     const lfoGain = engine.audio.synth.createLfo({
       depth: 1/3,
-      frequency: engine.utility.lerp(1/8, 1/4, protons),
+      frequency: engine.utility.lerp(4, 8, protons),
     }).connect(this.synth.param.gain)
 
-    this.synth.chainStop(lfoGain)
+    const lfoPwm = engine.audio.synth.createLfo({
+      depth: 0.2,
+      frequency: engine.utility.lerp(1/8, 1/4, protons),
+    }).connect(this.synth.param.width)
+
+    this.synth.chainStop(lfoGain).chainStop(lfoPwm)
   },
 })
