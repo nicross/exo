2515591d7fdae952cc7880c7cb75279350fdfb58
nicross <nicross@users.noreply.github.com>
2021-04-25 13:42:00 -0500

    Add basic reverb send

--- src/js/content/audio.js
+++ src/js/content/audio.js
@@ -1,6 +1,14 @@
 content.audio = (() => {
   const bus = engine.audio.mixer.createBus(),
-    context = engine.audio.context()
+    context = engine.audio.context(),
+    reverbInput = context.createGain(),
+    reverbSend = syngen.audio.mixer.send.reverb.create()
+
+  reverbSend.from(reverbInput).update({
+    x: 0,
+    y: 0,
+    z: 0,
+  })
 
   function createBus() {
     const gain = context.createGain()
@@ -27,6 +35,7 @@ content.audio = (() => {
       engine.audio.ramp.exponential(bus.gain, 1/64, 1/4)
       return this
     },
+    reverb: () => reverbInput,
   }
 })()
 
